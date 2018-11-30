07eeb1e43e900ca5aa41cdb4c915957e4d8aee47
izmirtais <izmirtais28@gmail.com>
2018-11-30 17:48:21 +0700

    membuat class ComboBox dan konstruktor

---
+++ src/com/izmir/transaksi/ComboBox.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.izmir.transaksi;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Izmir
+ */
+public class ComboBox {
+    private ArrayList<Item> items; //variabel untuk menyimpan objek item
+
+    // konstruktor combo box model
+    public ComboBox() {
+        this.items = new ArrayList<>();
+    }
+    
+    public Item[] toArray() {
+        Item[] items = new Item[this.items.size()];
+        for(int i = 0; i < items.length; i++) {
+            items[i] = this.items.get(i);
+        }
+        return items;
+    }
+}
