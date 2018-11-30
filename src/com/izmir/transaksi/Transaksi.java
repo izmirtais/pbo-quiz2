fe2061e0c45d63d2d32a7742ff5a797764567822
izmirtais <izmirtais28@gmail.com>
2018-11-30 16:26:45 +0700

    menambahkan atribut dan konstruktor pada class Transaksi

--- src/com/izmir/transaksi/Transaksi.java
+++ src/com/izmir/transaksi/Transaksi.java
@@ -5,10 +5,50 @@
  */
 package com.izmir.transaksi;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Izmir
  */
 public class Transaksi {
+        //variable untuk menampilkan kode pembayaran 
+    private final String kode;
+    //item untuk menampilkan item apa saja yang dibeli
+    private  ArrayList<Item> items = new ArrayList();
+    //variable menampilkan total pembelian
+    private float total;
     
+    //konstruktor
+
+    public Transaksi() {
+        this.kode = null;
+    }
+    
+
+    public Transaksi(String kode, ArrayList<Item> items) {
+        this.kode = kode;
+        this.items = items;
+    }
+    
+    //total setter
+    public void setTotal(){
+        float total = 0;
+        for(Item item : this.items){
+            total += item.getTotal();
+        }
+        this.total = total;
+    }
+    //output transaksi
+    public String Pembayaran(){
+        setTotal();
+        String obt = "";
+        obt += "Kode\t\t : " + this.kode + "\n";
+        obt += "Daftar Belanja : \n ";
+        for (Item item : this.items){
+            obt += "\t" + item.getNama()  + "(" + item.getJumlah() + ") : " +item.getTotal() + "\n" ;   
+        }
+        obt += "Total\t\t : " + this.total;
+        return obt;
+    }  
 }
