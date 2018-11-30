237d0086c0d7524ae34679af17a25461c6cb8dbd
izmirtais <izmirtais28@gmail.com>
2018-11-30 18:04:57 +0700

    membuat class TabelModel

---
+++ src/com/izmir/transaksi/TabelModel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.izmir.transaksi;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Izmir
+ */
+    public class TabelModel extends DefaultTableModel{
+        
+        private String[] kolom; // untuk menyimpan nama kolom
+}
